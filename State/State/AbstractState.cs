159e9ef11b2cd9141e72acd4139c500a6efd2f06
Eran Peled <eranp@r2net.com>
2019-06-16 16:40:53 +0300

    add Behavioral Patterns...

---
+++ State/State/AbstractState.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace State
+{
+    /// <summary>
+
+    /// The 'State' abstract class
+
+    /// </summary>
+
+    abstract class AbstractState
+
+    {
+        protected Account account;
+        protected double balance;
+
+        protected double interest;
+        protected double lowerLimit;
+        protected double upperLimit;
+
+        // Properties
+
+        public Account Account
+        {
+            get { return account; }
+            set { account = value; }
+        }
+
+        public double Balance
+        {
+            get { return balance; }
+            set { balance = value; }
+        }
+
+        public abstract void Deposit(double amount);
+        public abstract void Withdraw(double amount);
+        public abstract void PayInterest();
+    }
+}
